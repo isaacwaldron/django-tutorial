61e60bc19d93b7f7b195ea8b3a061e951af3a078
Isaac Waldron <isaac.waldron@gmail.com>
2018-07-16 22:31:02 +0000

    Part 5: Writing our first test
    Add a few simple tests for Question.was_published_recently() and fix a
    defect for Questions in the future.

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def run_was_published_recently_test(self, timeDelta, expected):
+        time = timezone.now() + timeDelta
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), expected)
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        self.run_was_published_recently_test(datetime.timedelta(days=30), False)
+
+    def test_was_published_recntly_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is less than one day old.
+        """
+        self.run_was_published_recently_test(datetime.timedelta(days=-1, seconds=1), True)
+
+    def test_was_published_recntly_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is more than one day old.
+        """
+        self.run_was_published_recently_test(datetime.timedelta(days=-1, seconds=-1), False)
